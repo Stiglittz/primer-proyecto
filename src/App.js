624d0e108d4870f6333fb3db0cf9017eeb76c4b7
Hugo Gzz <hgogzzs@gmail.com>
2021-04-23 13:39:38 -0500

    card component branch ej2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Price from 'components/price/Price'
 import './App.css'
-import Card from './components/card/Card'
-
+import Card from 'components/card/Card'
 
 function App() {
   return (
     <section className="container">
-      <Price />
+      <Card />
     </section>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,19 @@
-import icon from '../../images/card-icon.svg'
+import React from 'react';
+import icon from '../../images/icon.svg'
 import './Card.css'
 
 function Card(props) {
   return (
-    <div className="card">
-      <div className="card-icon">
+    <div className="card-container">
+      <div className="icon-background">
         <img src={icon} alt=""/>
       </div>
-      <div className="card-description">
-        <h1 className="card-tile">{props.title}</h1>
-        <p className="card-text">{props.text}</p>
+      <div className="title-description">
+        <h2>Popular Uses Of The Internet</h2>
+        <p>Many of us have at least been tempted at the flashy array of photo printers which seemingly leap off the shelves at even the least tech-savvy. </p>
       </div>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
